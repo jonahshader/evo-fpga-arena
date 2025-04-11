789146da838e209f641f49a103a5dccdb67fee6d
Jonah Shader <jonah.shader@gmail.com>
2025-04-11 01:59:07 -0400

    fix tests to match new paths

--- fpga/sim/tournament/test.py
+++ fpga/sim/tournament/test.py
@@ -18,10 +18,10 @@ lib = vu.add_library("lib")
 # Add testbench files
 # TODO: use glob for ../..
 lib.add_source_files([join(root, '*.vhd'),
-                     join(root, '../../tournament.vhd'),
-                     join(root, '../../ga_types.vhd'),
-                     join(root, '../../game_types.vhd'),
-                     join(root, '../../custom_utils.vhd')])
+                     join(root, '../../src/tournament.vhd'),
+                     join(root, '../../src/ga_types.vhd'),
+                     join(root, '../../src/game_types.vhd'),
+                     join(root, '../../src/custom_utils.vhd')])
 
 # Run vunit function
 vu.main()
--- software/to_verilog.sh
+++ software/to_verilog.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 # Uses GHDL to convert VHDL 2008 to Verilog using synth --out=verilog
-# Converts ../src_hdl/*.vhd to /ghdl_out/*.v
-# There's also content in ../src_hdl/imports/*.vhd
-# There are files in ../src_hdl/rom/*.rom, which aren't VHDL files, but are used by VHDL files
-# Top file is ../src_hdl/vampire_survivors_vhdl_main/vhdl/top_sim.vhd
-
-
-# ghdl -i --std=08 --workdir=ghdl_out ../src_hdl/*.vhd
-# ghdl -i --std=08 --workdir=ghdl_out ../src_hdl/imports/*.vhd
-# ghdl -i --std=08 --workdir=ghdl_out ../src_hdl/vampire_survivors_vhdl_main/vhdl/top_sim.vhd
-# ghdl -m --std=08 --workdir=ghdl_out top
-# ghdl synth --std=08 --workdir=ghdl_out --out=verilog top > ghdl_out/top.v
-
 # uses std_logic_unsigned, so -fsynopsys is needed
 # TODO: try removing -fsynopsys
 mkdir ghdl_out
 
-ghdl -i --std=08 -fsynopsys --workdir=ghdl_out ../fpga/*.vhd
-ghdl -i --std=08 -fsynopsys --workdir=ghdl_out ../fpga/imports/*.vhd
-ghdl -i --std=08 -fsynopsys --workdir=ghdl_out ../fpga/test/*.vhd
+ghdl -i --std=08 -fsynopsys --workdir=ghdl_out ../fpga/src/*.vhd
+ghdl -i --std=08 -fsynopsys --workdir=ghdl_out ../fpga/src/imports/*.vhd
+ghdl -i --std=08 -fsynopsys --workdir=ghdl_out ../fpga/src/verilator_tops/*.vhd
 ghdl -m --std=08 -fsynopsys --workdir=ghdl_out game_test
 ghdl synth --std=08 -fsynopsys --workdir=ghdl_out --out=verilog game_test > game_test.v
